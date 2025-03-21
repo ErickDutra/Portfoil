fb1b8bc059990e84c767ee0ec25788a39644eb7c
Erick Dutra <107477302+ErickDutra@users.noreply.github.com>
2025-03-21 15:08:35 -0300

    ajustes

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,95 +1,137 @@
-import { useEffect, useState } from 'react';
-import '../assets/css/projects.css';
-class ProjectItem {
-    name: string;
-    description: string;
-    link: string;
-    imagem: string;
+import { useEffect, useState } from "react";
+import "../assets/css/projects.css";
+import json from "../assets/projetos/projetos.json";
 
-    constructor(name: string, description: string, link: string, imagem: string) {
-        this.name = name;
-        this.description = description;
-        this.link = link;
-        this.imagem = imagem
-    }
+class ProjectItem {
+  titulo: string;
+  descricao: string;
+  ferramentas: string[];
+  link: string;
+  imagem: string;
+  constructor(
+    titulo: string,
+    descricao: string,
+    ferramentas: string[],
+    link: string,
+    imagem: string
+  ) {
+    this.titulo = titulo;
+    this.descricao = descricao;
+    this.ferramentas = ferramentas;
+    this.link = link;
+    this.imagem = imagem;
+  }
 }
 
 interface ProfileData {
-    itens: {
-        title: string;
-        descricao: string;
-        url: string;
-        imagem: string;
-    }[];
-}
-
-async function fetchProfileData(): Promise<ProfileData | null> {
-    try {
-        const response = await fetch(
-            "https://raw.githubusercontent.com/ErickDutra/New_repository/main/assets/data/profile.json"
-        );
-        if (!response.ok) {
-            throw new Error("Erro ao buscar os dados do perfil");
-        }
-        return await response.json();
-    } catch (error) {
-        console.error("Erro ao buscar os dados do perfil:", error);
-        return null;
-    }
+  itens: {
+    titulo: string;
+    descricao: string;
+    ferramentas?: string[];
+    link: string;
+    imagem: string;
+  }[];
 }
 
 function ProjectsPage() {
-    const [listProjects, setListProjects] = useState<ProjectItem[]>([]);
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [listProjects, setListProjects] = useState<ProjectItem[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [imageUrl, setImageUrl] = useState("");
 
-    useEffect(() => {
-        async function loadProjects() {
-            const response = await fetchProfileData();
-            if (response) {
-                const projects: ProjectItem[] = response.itens.map((item) =>
-                    new ProjectItem(item.title, item.descricao, item.url, item.imagem)
-                );
-                setListProjects(projects);
-            }
-        }
-        loadProjects();
-    }, []);
+  const images = import.meta.glob(
+    "../assets/projetos/image/*.{png,jpg,jpeg,svg}"
+  );
 
-    const handleNext = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % listProjects.length);
-    };
-
-    const handlePrev = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + listProjects.length) % listProjects.length);
-    };
-
-    if (listProjects.length === 0) {
-        return <div>Loading...</div>;
+  useEffect(() => {
+    function loadProjects() {
+      const response: ProfileData = json;
+      const projects: ProjectItem[] = response.itens.map(
+        (item) =>
+          new ProjectItem(
+            item.titulo,
+            item.descricao,
+            item.ferramentas || [],
+            item.link,
+            item.imagem
+          )
+      );
+      setListProjects(projects);
     }
+    loadProjects();
+  }, []);
 
+  useEffect(() => {
+    if (listProjects.length === 0) return;
     const currentProject = listProjects[currentIndex];
+    const path = `../assets/projetos/image/${currentProject.imagem}`;
+    if (images[path]) {
+      images[path]().then((module: any) => {
+        setImageUrl(module.default);
+      });
+    }
+  }, [listProjects, currentIndex, images]);
 
-    return (
-        <div id="projects" className="projects">
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % listProjects.length);
+  };
 
-            <div className="projects-list">
-                <div className="project-item">
-                    <div className="project-image"> 
-                        <img src={currentProject.imagem}  alt="icons" className='front' />
-                    </div>
-                    <h3>{currentProject.name}</h3>
-                    <p>{currentProject.description}</p>
-                    <a href={currentProject.link}><img width="80" height="80" src="https://img.icons8.com/color/100/000000/github.png" alt="github" /></a>
-                    <button id="left" onClick={handlePrev}>
-                        <img width="32" height="32" src="https://img.icons8.com/skeuomorphism/32/back.png" alt="back" />
-                    </button>
-                    <button id="right" onClick={handleNext}>
-                        <img width="32" height="32" src="https://img.icons8.com/skeuomorphism/32/forward.png" alt="forward" />
-                    </button>
-                </div>
-            </div>
-        </div>
+  const handlePrev = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + listProjects.length) % listProjects.length
     );
+  };
+
+  if (listProjects.length === 0) {
+    return <div>Loading...</div>;
+  }
+  const currentProject = listProjects[currentIndex];
+
+  return (
+    <div id="projects" className="projects ">
+      <div className="projects-list">
+        <div className="project-item">
+          <div className="project-container ">
+            <h3>{currentProject.titulo}</h3>
+            <div className="project-image">
+              {imageUrl && <img src={imageUrl} alt="icons" className="front" />}
+            </div>
+            <div className="project-footer">
+                <div className="ferramentas">
+                    {currentProject.ferramentas.map((item) => (
+                <img src={item} alt="" />
+              ))}
+                </div>
+              <p>{currentProject.descricao}</p>
+              <a href={currentProject.link}>
+                <img
+                  width="80"
+                  height="80"
+                  src="https://img.icons8.com/color/100/000000/github.png"
+                  alt="github"
+                />
+              </a>
+            </div>
+          </div>
+          <button id="left" onClick={handlePrev}>
+            <img
+              width="32"
+              height="32"
+              src="https://img.icons8.com/skeuomorphism/32/back.png"
+              alt="back"
+            />
+          </button>
+          <button id="right" onClick={handleNext}>
+            <img
+              width="32"
+              height="32"
+              src="https://img.icons8.com/skeuomorphism/32/forward.png"
+              alt="forward"
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectsPage;
